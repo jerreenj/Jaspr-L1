bc26cbd5a9164ade9d89e473eb452aac7016cfee
jerreenj <jerreen08@gmail.com>
2026-02-21 05:35:11 +0000

    auto-commit for f24fef17-d2e1-400d-8146-6900984a9c5d

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,6 +2,6 @@ export { default as Dashboard } from './Dashboard';
 export { default as BlockExplorer } from './BlockExplorer';
 export { default as Validators } from './Validators';
 export { default as WalletPage } from './WalletPage';
-export { default as DEXPage } from './DEXPage';
+export { default as StakingPage } from './StakingPage';
 export { default as SentinelPage } from './SentinelPage';
 export { default as MempoolPage } from './MempoolPage';
