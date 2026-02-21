34a6ac4c0b026236fea2e913586791257b7553b3
jerreenj <jerreen08@gmail.com>
2026-02-21 08:11:01 +0000

    auto-commit for a8e6764a-715a-4e7b-8c78-909811361dd7

--- backend/jasprchain/network/__init__.py
+++ backend/jasprchain/network/__init__.py
@@ -1,3 +1,7 @@
 from .mempool import Mempool, MempoolLane, MempoolStats
+from .p2p import P2PNetwork, Peer, P2PMessage, MessageType, GossipProtocol
 
-__all__ = ['Mempool', 'MempoolLane', 'MempoolStats']
+__all__ = [
+    'Mempool', 'MempoolLane', 'MempoolStats',
+    'P2PNetwork', 'Peer', 'P2PMessage', 'MessageType', 'GossipProtocol'
+]
