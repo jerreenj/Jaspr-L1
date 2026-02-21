24ce3bb71a03c95351b325e5203f3f4fb308f816
jerreenj <jerreen08@gmail.com>
2026-02-21 05:23:38 +0000

    auto-commit for 0f5d5de2-3908-4bde-a557-c688952f7445

--- backend/jasprchain/crypto/__init__.py
+++ backend/jasprchain/crypto/__init__.py
@@ -1,9 +1,9 @@
 from .keys import Ed25519KeyPair, generate_wallet
 from .bls import BLSSignature, aggregate_signatures, verify_aggregate
-from .hash import sha256_hash, merkle_root
+from .hash import sha256_hash, merkle_root, sha256_hex
 
 __all__ = [
     'Ed25519KeyPair', 'generate_wallet',
     'BLSSignature', 'aggregate_signatures', 'verify_aggregate',
-    'sha256_hash', 'merkle_root'
+    'sha256_hash', 'merkle_root', 'sha256_hex'
 ]
