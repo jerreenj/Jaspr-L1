df30fc9a0fc2ce51fa187f04f872f02dc0f5acba
jerreenj <jerreen08@gmail.com>
2026-02-21 12:03:44 +0000

    auto-commit for fa03ebee-d601-4b93-9fd8-f836ca1e3173

---
+++ rust-core/target/debug/deps/jaspr_move_vm-8e5688345e59da0f.d
@@ -0,0 +1,9 @@
+/app/rust-core/target/debug/deps/jaspr_move_vm-8e5688345e59da0f.d: crates/jaspr-move-vm/src/lib.rs crates/jaspr-move-vm/src/adapter.rs crates/jaspr-move-vm/src/module_cache.rs crates/jaspr-move-vm/src/stdlib.rs crates/jaspr-move-vm/src/types.rs
+
+/app/rust-core/target/debug/deps/jaspr_move_vm-8e5688345e59da0f: crates/jaspr-move-vm/src/lib.rs crates/jaspr-move-vm/src/adapter.rs crates/jaspr-move-vm/src/module_cache.rs crates/jaspr-move-vm/src/stdlib.rs crates/jaspr-move-vm/src/types.rs
+
+crates/jaspr-move-vm/src/lib.rs:
+crates/jaspr-move-vm/src/adapter.rs:
+crates/jaspr-move-vm/src/module_cache.rs:
+crates/jaspr-move-vm/src/stdlib.rs:
+crates/jaspr-move-vm/src/types.rs:
---
+++ rust-core/target/debug/deps/jaspr_state-f3bfca3504970a6b.d
@@ -0,0 +1,9 @@
+/app/rust-core/target/debug/deps/jaspr_state-f3bfca3504970a6b.d: crates/jaspr-state/src/lib.rs crates/jaspr-state/src/db.rs crates/jaspr-state/src/account_store.rs crates/jaspr-state/src/block_store.rs crates/jaspr-state/src/state_tree.rs
+
+/app/rust-core/target/debug/deps/jaspr_state-f3bfca3504970a6b: crates/jaspr-state/src/lib.rs crates/jaspr-state/src/db.rs crates/jaspr-state/src/account_store.rs crates/jaspr-state/src/block_store.rs crates/jaspr-state/src/state_tree.rs
+
+crates/jaspr-state/src/lib.rs:
+crates/jaspr-state/src/db.rs:
+crates/jaspr-state/src/account_store.rs:
+crates/jaspr-state/src/block_store.rs:
+crates/jaspr-state/src/state_tree.rs:
