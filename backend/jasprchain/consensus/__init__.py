35e41a2fc62d139f6a30b57b1f85cd9e0d7cc422
jerreenj <jerreen08@gmail.com>
2026-02-21 08:10:48 +0000

    auto-commit for b4e82056-194c-47b0-a2ff-5790c7375972

--- backend/jasprchain/consensus/__init__.py
+++ backend/jasprchain/consensus/__init__.py
@@ -2,10 +2,12 @@ from .block import Block, BlockHeader, create_genesis_block
 from .validator import Validator, ValidatorSet
 from .proposer import ProposerSelection, VRFOutput
 from .finality import FinalityEngine, CommitteeAttestation
+from .slashing import SlashingModule, SlashingType, SlashingEvidence, SlashingRecord
 
 __all__ = [
     'Block', 'BlockHeader', 'create_genesis_block',
     'Validator', 'ValidatorSet', 
     'ProposerSelection', 'VRFOutput',
-    'FinalityEngine', 'CommitteeAttestation'
+    'FinalityEngine', 'CommitteeAttestation',
+    'SlashingModule', 'SlashingType', 'SlashingEvidence', 'SlashingRecord'
 ]
