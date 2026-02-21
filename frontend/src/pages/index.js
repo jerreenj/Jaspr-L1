da9d31bba9a7546f817aeb9795fa9cee1dd38ec3
jerreenj <jerreen08@gmail.com>
2026-02-21 05:22:52 +0000

    auto-commit for 8e7ec9e5-517f-4643-b253-be30d68ee861

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+export { default as Dashboard } from './Dashboard';
+export { default as BlockExplorer } from './BlockExplorer';
+export { default as Validators } from './Validators';
+export { default as WalletPage } from './WalletPage';
+export { default as DEXPage } from './DEXPage';
+export { default as SentinelPage } from './SentinelPage';
+export { default as MempoolPage } from './MempoolPage';
