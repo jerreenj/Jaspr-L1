2a05745ddde935be1e9a5bf3904ebcb89fff4a52
jerreenj <jerreen08@gmail.com>
2026-02-21 08:11:21 +0000

    auto-commit for 46599e55-d02a-4ca5-9a32-37d58afa3e0a

--- backend/jasprchain/execution/__init__.py
+++ backend/jasprchain/execution/__init__.py
@@ -1,7 +1,9 @@
 from .parallel import ParallelExecutor, ExecutionResult, ConflictSet
 from .transaction import Transaction, TransactionType, SignedTransaction
+from .move_vm import MoveVM, MoveModule, MoveResource, MoveFunction
 
 __all__ = [
     'ParallelExecutor', 'ExecutionResult', 'ConflictSet',
-    'Transaction', 'TransactionType', 'SignedTransaction'
+    'Transaction', 'TransactionType', 'SignedTransaction',
+    'MoveVM', 'MoveModule', 'MoveResource', 'MoveFunction'
 ]
